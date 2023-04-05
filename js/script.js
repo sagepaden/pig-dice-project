0357db2ecaf24cd627d3e3b1998cf88967976ee1
Sage Paden <sage.paden@gmail.com>
2023-04-05 11:47:41 -0700

    added and tested player switching and turn score functionality. Going to lunch
    Co-authored-by: Max Betich <BetichM@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,29 +1,48 @@
+// Player Logic
+
 function Player(name) {
   this.name = name;
   this.score = 0;
 }
+
+let player1 = new Player("player1");
+let player2 = new Player("player2");
+
+
+
+// Game Logic
+
+function Game() {  //possibly needs player1, player2
+  this.players = [player1, player2];
+  this.activePlayer = player1; // maybe something different later
+  this.currentScore = 0;
+  this.gameover = false;
+}
+
+Game.prototype.switchPlayers = function() {
+  this.activePlayer = this.activePlayer === player1? player2 : player1;
+};
+
+Game.prototype.isGameover = function() {
+  if (newGame.currentScore === 0) {
+    newGame.switchPlayers();
+  } else if (this.currentScore + this.activePlayer.score >= 100) {
+    console.log(this.activePlayer.name + " is the winner!");
+  }
+  //return activePlayer.score >= 100;
+};
+
+
+
+//Floating Functions
+
+let newGame = new Game();
 
 function diceRoller() {
   const min = Math.ceil(1);
   const max = Math.floor(6);
   return  Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-let player1 = new Player("player1");
-let player2 = new Player("player2");
-
-function Game() {  //possibly needs player1, player2
-  this.players = [player1, player2];
-  this.activePlayer = 0; // maybe something different later
-  this.currentScore = 0;
-  this.gameover = false;
-}
-Game.prototype.gameover = function() {
-  return activePlayer.score >= 100;
-}
-
-//let currentScore;
-let newGame = new Game();
 
 function turnScore() {  //possibly needs event in argument
   //event.preventDefault();
@@ -34,6 +53,18 @@ function turnScore() {  //possibly needs event in argument
   } else {
     newGame.currentScore = newGame.currentScore + roll;
   }
+  newGame.isGameover();
+  console.log(newGame.activePlayer);
   return newGame.currentScore;
+};
+
+function holdScore() {
+  newGame.activePlayer.score = newGame.activePlayer.score + newGame.currentScore;
+  
 }
+
+//let currentScore;
+
+
+
 
